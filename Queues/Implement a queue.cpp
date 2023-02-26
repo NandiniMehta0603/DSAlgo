4908b0baa5fd21ac3a3b0de484d002edcd987ef1
Nandini Mehta <84312981+NandiniMehta0603@users.noreply.github.com>
2023-02-26 19:16:55 +0530

    Easy

---
+++ Queues/Implement a queue.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h> 
+class Queue {
+    int *arr;
+    int qfront;
+    int rear;
+    int size;
+public:
+    Queue() {
+        size=1000001;
+        arr=new int[size];
+        qfront=0;
+        rear=0;
+    }
+
+    /*----------------- Public Functions of Queue -----------------*/
+
+    bool isEmpty() {
+        if(rear==qfront){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    void enqueue(int data) {
+        if(rear==size){
+            return ;
+        }
+        else{
+            arr[rear]=data;
+            rear++;
+        }
+    }
+
+    int dequeue() {
+        if(qfront==rear){
+            return -1;
+        }
+        else{
+            int val=arr[qfront];
+            arr[qfront]=-1;
+            qfront++;
+            if(qfront==rear){
+                qfront=0;
+                rear=0;
+            }
+            return val;
+        }
+    }
+
+    int front() {
+        if(qfront==rear){
+            return -1;
+        }
+        else{
+            return arr[qfront];
+        }
+        // Implement the front() function
+    }
+};
