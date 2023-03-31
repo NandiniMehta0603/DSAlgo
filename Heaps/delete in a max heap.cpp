48c42067bcfbb9b29481a3bb4b8463a2dd12292d
Nandini Mehta <84312981+NandiniMehta0603@users.noreply.github.com>
2023-03-31 16:16:18 +0530

    Update delete in a max heap.cpp

--- Heaps/delete in a max heap.cpp
+++ Heaps/delete in a max heap.cpp
@@ -46,15 +46,15 @@ class heap{
         
         //take root node to its correct position
         int i=1;
-        while(i<size){
+        while(i<=size){
             int leftIndex=2*i;
             int rightIndex=2*i+1;
             
-            if(leftIndex<size && arr[i]<arr[leftIndex]){
+            if(leftIndex<=size && arr[i]<arr[leftIndex]){
                 swap(arr[i],arr[leftIndex]);
                 i=leftIndex;
             }
-            else if(rightIndex<size && arr[i]<arr[rightIndex]){
+            else if(rightIndex<=size && arr[i]<arr[rightIndex]){
                 swap(arr[i],arr[rightIndex]);
                 i=rightIndex;
             }
