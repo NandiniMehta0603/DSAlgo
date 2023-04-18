35aadc7ccc0a78429f714c05a04eeb6e2fa0fc02
Nandini Mehta <84312981+NandiniMehta0603@users.noreply.github.com>
2023-04-18 11:19:17 +0530

    Easy

--- Graphs/Create a graph.cpp
+++ Graphs/Create a graph.cpp
@@ -1,47 +1,51 @@
 // Online C++ compiler to run C++ program online
 #include <iostream>
-#include<unordered_map>
-#include<list>
+#include <list>
+#include <unordered_map>
 using namespace std;
 template <typename T>
 class Graph{
     public:
     unordered_map<T,list<T>> adj;
     void addEdge(T u,T v,bool direction){
-        //direction=0 means undirected graph
-        //direction=1 means directed graph
-        
-        //create an edge from u to v
         adj[u].push_back(v);
-        if(direction==0){
+        if(direction==0)
             adj[v].push_back(u);
-        }
     }
-    void printAdjList(){
+    void printAdj(){
         for(auto i:adj){
-            cout<<i.first<<"->";
+            cout<<"List for "<<i.first<<" is: ";
             for(auto j:i.second){
                 cout<<j<<", ";
             }cout<<endl;
         }
     }
 };
+
+
 int main() {
     int n;
-    cout<<"Enter the no of nodes"<<endl;
+    cout<<"Enter the no of nodes: "<<endl;
     cin>>n;
     int m;
-    cout<<"Enter the no of edges"<<endl;
+    cout<<"Enter the no of edges: "<<endl;
     cin>>m;
-    Graph<int>g;
+    Graph<int> g;
+    // g.addEdge(1,2,0);
+    // g.addEdge(2,3,0);
+    // g.addEdge(1,4,0);
+    // g.addEdge(4,3,0);
+    
     for(int i=0;i<m;i++){
-        int u,v;
+        cout<<"For edge "<<i<<endl;
+        int u;
+        int v;
+        cout<<"Enter the value of u and v "<<endl;
         cin>>u>>v;
-        //make an undirected graph
         g.addEdge(u,v,0);
     }
-    //print the graph
-    g.printAdjList();
+    
+    g.printAdj();
 
     return 0;
 }
